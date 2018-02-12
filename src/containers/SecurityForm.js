91f3fe375b078a932e51ec0b56574d2ebdcf0cf8
Linus Bein Fahlander <linus.webdevelopment@gmail.com>
2018-02-12 13:15:52 +0100

    Cleaned up directory and create root component

---
+++ src/containers/SecurityForm.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Linus Bein Fahlander on 2018-02-12.
+ */
+import React, { Component } from 'react';
+
+class SecurityForm extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <div className="sf-main">
+            </div>
+        );
+    }
+}
+
+export default SecurityForm;
