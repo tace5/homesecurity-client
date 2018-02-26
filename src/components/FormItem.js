a2fa1e89f41b06ab4a994e800712a62c28e37834
LinusBF <linus.webdevelopment@gmail.com>
2018-02-26 12:59:55 +0100

    Started work on getting current settings from API

--- src/components/FormItem.js
+++ src/components/FormItem.js
@@ -2,14 +2,28 @@
  * Created by Linus Bein Fahlander on 2018-02-12.
  */
 import React, { Component } from 'react';
+import API_SETTINGS from '../utils/ApiSettings';
 
 class FormItem extends React.Component {
     constructor(props) {
         super(props);
+        this.request = require("request");
+        this.state = {
+            currentSettings: null
+        }
     }
 
-    get_current_setting(setting){
-
+    get_current_setting(){
+        //Get current settings from API
+        let result = [];
+        this.request(API_SETTINGS['api']['root'] + this.props.setting['api_uid'] + ".json", function (error, response, body) {
+            let data = JSON.parse(body);
+            data.items.forEach(function (item) {
+                result.push({id: item['id'], value: item['value']});
+            });
+        });
+        console.log(result);
+        return result;
     }
 
     send_post_request(event){
@@ -20,16 +34,20 @@ class FormItem extends React.Component {
 
     }
 
-    render(){
-        const currentSetting = this.get_current_setting(this.props.setting).map((setting, i) => {
-            return <div id={"set-" + setting.id} className="current_setting">{setting.name}</div>
+    componentDidMount(){
+        // TODO - Call get_current_setting and update state
+        const currentSetting = this.get_current_setting().map((cur_setting, i) => {
+            console.log(cur_setting);
+            return <div id={"set-" + cur_setting.id} className="current_setting">{cur_setting.value}</div>
         });
+    }
 
+    render(){
         return (
             <div className="sf-setting">
                 <h3>{this.props.setting.display.title}</h3>
                 <div className="sf-setting-current">
-                    {}
+                    {this.state.currentSettings}
                 </div>
             </div>
         );
